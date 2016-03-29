6dff3b01648a86dd6046e8cf6c346fc59394aaa0
Matt McCormick <matt@mbmccormick.com>
2016-03-28 18:11:30 -0700

    Publishing to npm.

--- example.js
+++ example.js
@@ -1,16 +1,16 @@
-var express = require("express"),
+var express = require('express'),
     app = express();
 
-var AutomaticApiClient = require("automatic-node"),
-    client = new AutomaticApiClient("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET");
+var AutomaticApiClient = require('./automatic-api-client'),
+    client = new AutomaticApiClient('YOUR_CLIENT_ID', 'YOUR_CLIENT_SECRET');
 
-app.get("/authorize", function (req, res) {
-    res.redirect(client.getAuthorizeUrl('scope:public scope:user:profile scope:location scope:current_location scope:vehicle:location scope:vehicle:events scope:vehicle:vin scope:trip scope:behavior', 'YOUR_CALLBACK_URL'));
+app.get('/authorize', function (req, res) {
+    res.redirect(client.getAuthorizeUrl('scope:public scope:user:profile scope:location scope:vehicle:profile scope:vehicle:events scope:trip scope:behavior', 'YOUR_CALLBACK_URL'));
 });
 
-app.get("/callback", function (req, res) {
+app.get('/callback', function (req, res) {
     client.getAccessToken(req.query.code, 'YOUR_CALLBACK_URL').then(function (result) {
-        client.get("/trip/", result.access_token).then(function (results) {
+        client.get('/trip/', result.access_token).then(function (results) {
             res.send(results[0]);
         });
     }).catch(function (error) {
